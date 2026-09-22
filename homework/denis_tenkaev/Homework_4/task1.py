3175c510d49faca5d61b9b3b57803d98ddea9736
Denis Tenkaev <medennis86@gmail.com>
2026-09-22 12:37:29 +0300

    Corrected syntax for tuple as a key

--- homework/denis_tenkaev/Homework_4/task1.py
+++ homework/denis_tenkaev/Homework_4/task1.py
@@ -8,7 +8,7 @@ my_dict = {"tuple": ("yes", "no", 3, 42, 10),
 print(my_dict["tuple"][-1])  # Выводим последний элемент для ключа "tuple"
 my_dict["list"].append("mouse")  # Добавляем еще один элемент в конец ключа "list"
 my_dict["list"].pop(1)  # Удаляем второй элемент списка
-my_dict["dict"]["(i am a tuple,)"] = "mouse"  # Добавляем новый элемент для ключа "dict"
+my_dict["dict"][("i am a tuple",)] = "mouse"  # Добавляем новый элемент для ключа "dict"
 my_dict["dict"].pop("car")  # Удаляем элемент из ключа "dict"
 my_dict["set"].add(8)  # Добавляем еще один элемент в ключ "set"
 my_dict["set"].pop()  # Удаляем элемент из ключа "set"
