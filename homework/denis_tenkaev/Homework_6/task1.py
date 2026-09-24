a0d780367d90af3ad75611a54065388cb95f2ced
planetfunk64 <medennis86@gmail.com>
2026-09-24 22:04:23 +0300

    Homework_6 done

---
+++ homework/denis_tenkaev/Homework_6/task1.py
@@ -0,0 +1,10 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+text = text.split()
+for word in text:
+        if word.endswith(","):
+            print(word.rstrip((",")) + "ing", end=", ")
+        elif word.endswith("."):
+            print(word.rstrip((".")) + "ing", end=". ")
+        else:
+            word = word + "ing"
+            print(word, end=" ")
