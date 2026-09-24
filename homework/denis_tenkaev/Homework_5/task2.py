2207b7bd695bed13328eb7937bdfa2915b67f8ff
planetfunk64 <medennis86@gmail.com>
2026-09-24 09:27:50 +0300

    Added another method in task2

--- homework/denis_tenkaev/Homework_5/task2.py
+++ homework/denis_tenkaev/Homework_5/task2.py
@@ -12,3 +12,17 @@ text = "результат работы программы: 9"
 index = (text.find(':')) + 1
 result = int(text[index:]) + 10
 print(result)
+
+# Еще один способ вчера в голову пришел, после того как запушил
+
+text = "результат операции: 42"
+result = int(text.split()[-1]) + 10
+print(result)
+
+text = "результат операции: 514"
+result = int(text.split()[-1]) + 10
+print(result)
+
+text = "результат работы программы: 9"
+result = int(text.split()[-1]) + 10
+print(result)
