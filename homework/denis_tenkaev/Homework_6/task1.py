b9fdffa4e5eb617b84cfcee27769402ce7aeddfe
planetfunk64 <medennis86@gmail.com>
2026-09-24 22:14:26 +0300

    Corrected after Lint check

--- homework/denis_tenkaev/Homework_6/task1.py
+++ homework/denis_tenkaev/Homework_6/task1.py
@@ -1,10 +1,13 @@
-text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+text = (
+    "Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
+    " Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+    )
 text = text.split()
 for word in text:
-        if word.endswith(","):
-            print(word.rstrip((",")) + "ing", end=", ")
-        elif word.endswith("."):
-            print(word.rstrip((".")) + "ing", end=". ")
-        else:
-            word = word + "ing"
-            print(word, end=" ")
+    if word.endswith(","):
+        print(word.rstrip((",")) + "ing", end=", ")
+    elif word.endswith("."):
+        print(word.rstrip((".")) + "ing", end=". ")
+    else:
+        word = word + "ing"
+        print(word, end=" ")
